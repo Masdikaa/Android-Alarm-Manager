7f35a565b7534fbe2ac60c38299ffbe3c3989535
Masdikaa <masdikailhanmansiz@gmail.com>
2024-05-29 22:49:41 +0700

    Create Alarm Activities and Notification Permission

---
+++ app/src/main/java/com/masdika/alarmmanager/AlarmReceiver.kt
@@ -0,0 +1,41 @@
+package com.masdika.alarmmanager
+
+import android.Manifest
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
+class AlarmReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        //Execute this code when alarm is trigger
+        val i = Intent(context, DestinationActivity::class.java)
+        intent!!.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+
+        val pendingIntent = PendingIntent.getActivity(context, 0, i, PendingIntent.FLAG_IMMUTABLE)
+        val builder = NotificationCompat.Builder(context!!, "Masdika")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle("Tes Alarm App")
+            .setContentText("Alarm App")
+            .setAutoCancel(true)
+            .setDefaults(NotificationCompat.DEFAULT_ALL)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+
+        val notificationManager = NotificationManagerCompat.from(context)
+        if (ActivityCompat.checkSelfPermission(
+                context,
+                Manifest.permission.POST_NOTIFICATIONS
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            Toast.makeText(context, "Permission Denied", Toast.LENGTH_LONG).show()
+            return
+        }
+        notificationManager.notify(123, builder.build())
+    }
+}
--- app/src/main/java/com/masdika/alarmmanager/MainActivity.kt
+++ app/src/main/java/com/masdika/alarmmanager/MainActivity.kt
@@ -1,15 +1,31 @@
 package com.masdika.alarmmanager
 
+import android.app.AlarmManager
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.icu.util.Calendar
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 import com.masdika.alarmmanager.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var timePicker: MaterialTimePicker
+    private lateinit var calendarPicker: Calendar
+    private lateinit var alarmManager: AlarmManager
+    private lateinit var pendingIntent: PendingIntent
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,5 +36,85 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        createNotificationChannel()
+
+        binding.setTimeButton.setOnClickListener(this)
+
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name: CharSequence = "Emergency Alarm"
+            val importance = NotificationManager.IMPORTANCE_HIGH
+            val channel = NotificationChannel("Masdika", name, importance)
+            channel.description = "Wakey Wakey"
+            val notificationManager = getSystemService(
+                NotificationManager::class.java
+            )
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.setTimeButton -> showTimePicker()
+            R.id.setAlarmButton -> setAlarm()
+            R.id.cancelAlarmButton -> cancelAlarm()
+        }
+    }
+
+    private fun showTimePicker() {
+        timePicker = MaterialTimePicker.Builder()
+            .setTimeFormat(TimeFormat.CLOCK_24H)
+            .setHour(12)
+            .setMinute(0)
+            .setTitleText("Select Alarm Time")
+            .build()
+        timePicker.show(supportFragmentManager, "Masdika")
+
+        timePicker.addOnPositiveButtonClickListener {
+            if (timePicker.hour > 12) {
+                binding.dateTimeTV.text =
+                    String.format("%02d", timePicker.hour - 12) + " : " + String.format(
+                        "%02d",
+                        timePicker.minute
+                    ) + " PM"
+            } else {
+                String.format("%02d", timePicker.hour) + " : " + String.format(
+                    "%02d",
+                    timePicker.minute
+                ) + "AM"
+            }
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                calendarPicker = Calendar.getInstance()
+                calendarPicker[Calendar.HOUR_OF_DAY] = timePicker.hour
+                calendarPicker[Calendar.MINUTE] = timePicker.minute
+                calendarPicker[Calendar.SECOND] = 0
+                calendarPicker[Calendar.MILLISECOND] = 0
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    private fun setAlarm() {
+        alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this, AlarmReceiver::class.java)
+        pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        alarmManager.setRepeating(
+            AlarmManager.RTC_WAKEUP, calendarPicker.timeInMillis,
+            AlarmManager.INTERVAL_DAY, pendingIntent
+        )
+
+        Toast.makeText(this, "Alarm Set", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun cancelAlarm() {
+        alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        val intent = Intent(this, AlarmReceiver::class.java)
+        pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+        alarmManager.cancel(pendingIntent)
+        Toast.makeText(this, "Alarm cancel", Toast.LENGTH_SHORT).show()
     }
 }
